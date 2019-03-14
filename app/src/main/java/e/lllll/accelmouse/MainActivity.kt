12a4ae5eab0e76aef63d0f232ddda79221c0e4f9
yuki tanoue <llllllllllv0@gmail.com>
2019-03-14 23:55:14 +0900

    ある程度作った

--- app/src/main/java/e/lllll/accelmouse/MainActivity.kt
+++ app/src/main/java/e/lllll/accelmouse/MainActivity.kt
@@ -6,11 +6,30 @@ import android.os.Bundle
 import android.view.MotionEvent
 import e.lllll.accelmouse.data.Mouse
 import android.app.Activity
+import android.content.Context
 import android.graphics.Point
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
+import android.util.Log
 
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), SensorEventListener {
+
     private val mouse = Mouse()
+
+    /**
+     * Androidセンサを使用するために必要。
+     * 本プロパティにアクセスされたときに初めて処理が実行される
+     */
+    private val sensorManager: SensorManager by lazy {
+        getSystemService(Context.SENSOR_SERVICE) as SensorManager
+    }
+
+    private val gyroSensor by lazy {
+        sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,18 +50,60 @@ class MainActivity : AppCompatActivity() {
         return point
     }
 
+    private fun registerSensors() {
+        sensorManager.registerListener(this, gyroSensor, SensorManager.SENSOR_DELAY_GAME)
+    }
+
+    /**
+     * タッチイベントを処理する
+     */
     override fun onTouchEvent(event: MotionEvent?): Boolean {
         if (event == null) {
+            Log.d("onTouchEvent", "event is null")
             return super.onTouchEvent(event)
         }
+        val x = event.x
+        val y = event.y
 
         when (event.action) {
             MotionEvent.ACTION_DOWN -> {
                 mouse.setClick()
             }
+            MotionEvent.ACTION_UP -> {
+                mouse.releaseClick()
+            }
+            MotionEvent.ACTION_MOVE -> {
+                mouse.setWheelDistance(y)
+            }
         }
-        mouse.touchX = event.x
-        mouse.touchY = event.y
+        mouse.touchX = x
+        mouse.touchY = y
         return super.onTouchEvent(event)
     }
+
+    /**
+     * センサーのイベントを処理する
+     */
+    override fun onSensorChanged(event: SensorEvent?) {
+
+        if (event == null) {
+            Log.d("onSensorChanged", "sensor is null")
+            return
+        }
+        when (event.sensor.type) {
+            Sensor.TYPE_GYROSCOPE -> {
+                // 画面のX軸はジャイロスコープのX軸回転を利用
+                // 画面のY軸はジャイロスコープのZ軸回転を利用
+                mouse.x = event.values[0]
+                mouse.y = event.values[2]
+            }
+        }
+    }
+    /**
+     * センサーが変化したときのイベントを処理する。
+     */
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+
+    }
+
 }
--- app/src/main/java/e/lllll/accelmouse/data/Mouse.kt
+++ app/src/main/java/e/lllll/accelmouse/data/Mouse.kt
@@ -1,14 +1,22 @@
 package e.lllll.accelmouse.data
 
+import se.ansman.kotshi.JsonSerializable
+
+@JsonSerializable
 data class Mouse(
     // マウスカーソルの座標
-    var x: Int = 0,
-    var y: Int = 0,
+    var x: Float = 0F,
+    var y: Float = 0F,
 
+    // 左右クリックを管理
     var clicked: Int = 0,
+    val noClick: Int = 0,
     val leftClick: Int = 1,
-    val rightClick: Int  = 2,
-    val middleClick: Int  = 3,
+    val rightClick: Int = 2,
+    val middleClick: Int = 3,
+
+    // ホイール距離
+    var wheel: Float = 0F,
 
     // タッチした画面の座標
     var touchX: Float = 0F,
@@ -18,7 +26,7 @@ data class Mouse(
     var displayWidth: Int = 0,
     var displayHeight: Int = 0
 ) {
-    fun setClick(): Unit {
+    fun setClick() {
         if (isOnLeftClickArea()) {
             clicked = leftClick
         }
@@ -26,6 +34,20 @@ data class Mouse(
             clicked = rightClick
         }
     }
+
+    fun releaseClick() {
+        clicked = noClick
+        wheel = 0F
+    }
+
+    fun setWheelDistance(lastTouchY: Float) {
+        if (touchX > displayWidth * 4 / 9 && touchX < displayWidth * 5 / 9) {
+            wheel = touchY - lastTouchY
+            return
+        }
+        wheel = 0F
+    }
+
     private fun isOnLeftClickArea(): Boolean {
         val width = displayWidth as Float
         return touchX < width * 4 / 9
